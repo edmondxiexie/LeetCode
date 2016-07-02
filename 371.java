52b5c9dbbc5e288326b8ab0b6dd87dc51e4a3810
Edmond Xie <xiejy36@gmail.com>
2016-07-02 11:57:03 -0400

    add question 160

---
+++ 371.java
@@ -0,0 +1,14 @@
+// 371. Sum of Two Integers
+// Calculate the sum of two integers a and b, but you are not allowed to use 
+// the operator + and -.
+
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b; // add
+            b = carry << 1; // move carry to next bit
+        }
+        return a;
+    }
+}
