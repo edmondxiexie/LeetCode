c210fcf87e0d787327e9bdc34e72213e6a217db3
Edmond Xie <xiejy36@gmail.com>
2016-07-02 08:41:42 -0400

    add question 198

---
+++ 198.java
@@ -0,0 +1,31 @@
+// 198. House Robber
+
+// You are a professional robber planning to rob houses along a street. 
+// Each house has a certain amount of money stashed, the only constraint 
+// stopping you from robbing each of them is that adjacent houses have security 
+// system connected and it will automatically contact the police if two 
+// adjacent houses were broken into on the same night.
+
+// Given a list of non-negative integers representing the amount of money of 
+// each house, determine the maximum amount of money you can rob tonight 
+// without alerting the police.
+
+import java.lang.Math;
+
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int with = nums[0];
+        int without = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int newWith = without + nums[i];
+            int newWithout = Math.max(with, without);
+            with = newWith;
+            without = newWithout;
+        }
+        return Math.max(with, without);
+
+    }
+}
