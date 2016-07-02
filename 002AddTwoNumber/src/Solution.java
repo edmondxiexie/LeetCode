3772229a898e806b43da785d9a8ccd316fc73e16
Edmond Xie <xiejy36@gmail.com>
2016-07-01 21:08:17 -0400

    add question 002

---
+++ 002AddTwoNumber/src/Solution.java
@@ -0,0 +1,44 @@
+
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { 
+            val = x;
+        }
+    }
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = 0;
+        int carry = 0;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        while (l1 != null || l2 != null) {
+            int x = (p1 != null) ? p1.val : 0;
+            int y = (p2 != null) ? p2.val : 0;
+            sum = x + y + carry;
+            carry =  sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p1 != null) {
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            }           
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }
+}
