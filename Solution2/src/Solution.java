af36e27d107c8228a7cc2c8ac4dc5cb8ddda24cb
Jiayi Xie <Edmond@JiayideMacBook-Pro.local>
2016-09-07 02:22:23 -0400

    add question 107

--- Solution2/src/Solution.java
+++ Solution2/src/Solution.java
@@ -353,6 +353,34 @@ public class Solution {
         return depth;
     }
     
+    // 107. Binary Tree Level Order Traversal II
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }            
+            result.add(level);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+    
+    
     public static void main(String[] args) {
 //        int[] nums = {6, 5, 2, 4, 3, 1};
         int[] nums = {1, 1};
